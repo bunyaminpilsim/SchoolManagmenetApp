ece0b4ab3296d969cf15d8351991c4dcbcbd006b
Bunyaminpilsim <bunyaminpilsim@gmail.com>
2024-06-19 23:39:19 +0300

    first commit

---
+++ SchoolManagmenetApp/Models/Role.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolManagmenetApp.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public List<Person>? Persons { get; set; }
+
+    }
+}
