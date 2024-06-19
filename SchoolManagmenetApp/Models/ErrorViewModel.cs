5f6fefb10474782fabf0ed6895c667e4d35b64ca
Bunyaminpilsim <bunyaminpilsim@gmail.com>
2024-06-19 23:18:02 +0300

    Update .gitignore to exclude .vs folder

--- SchoolManagmenetApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SchoolManagmenetApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
